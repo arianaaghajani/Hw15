ec4bab61fe286c1b40ca072459d04bff5c3d4e8f
arianaaghajani <aghajaniariana@gmail.com>
2024-03-31 11:23:00 +0430

    add rationalTest class

---
+++ src/test/java/RationalTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RationalTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void sub() {
+    }
+
+    @Test
+    void mul() {
+    }
+
+    @Test
+    void div() {
+    }
+}
