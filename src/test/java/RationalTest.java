a4b52223fe73b6622f48ffaa5d7ae5ced3181e13
arianaaghajani <aghajaniariana@gmail.com>
2024-03-31 11:39:13 +0430

    test add method

--- src/test/java/RationalTest.java
+++ src/test/java/RationalTest.java
@@ -5,10 +5,18 @@ class RationalTest {
 
     @Test
     void add() {
+        Rational rational1=new Rational(1,2);
+        Rational rational2=new Rational(1,3);
+
+        Rational result =rational1.add(rational2);
+
+        assertEquals(5,result.getNumerator());
+        assertEquals(6,result.getDenominator());
     }
 
     @Test
     void sub() {
+
     }
 
     @Test
