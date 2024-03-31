726130f4bbe5c84517a7b58d7bafb7ae8169219b
arianaaghajani <aghajaniariana@gmail.com>
2024-03-31 11:09:20 +0430

    edit constructor

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -4,6 +4,14 @@ public class Rational {
 
     public Rational(int numerator, int denominator) {
         this.numerator = numerator;
+        if (denominator==0){
+            throw new IllegalArgumentException("denominator can not be zero");
+        }
         this.denominator = denominator;
     }
+
+    //public Rational(int numerator,int denominator){
+
+    }
+
 }
