30d4052907e8fc42bfa17d69f36f7dfb60276921
arianaaghajani <aghajaniariana@gmail.com>
2024-03-31 11:06:08 +0430

    add constructor

--- src/main/java/Rational.java
+++ src/main/java/Rational.java
@@ -1,2 +1,9 @@
 public class Rational {
+    int numerator;
+    int denominator;
+
+    public Rational(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
 }
