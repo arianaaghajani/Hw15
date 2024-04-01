eee70d603f621c03e80a264e72988166ff4cb677
arianaaghajani <aghajaniariana@gmail.com>
2024-04-01 20:54:53 +0430

    add to string and constructor

--- src/main/java/ir/ariana/q2/Person.java
+++ src/main/java/ir/ariana/q2/Person.java
@@ -1,23 +1,28 @@
 package ir.ariana.q2;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Entity
 public class Person {
     @Id
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
     private String lastName;
     private String birthDate;
 
-
+    public Person(String name, String lastName, String birthDate) {
+        this.name = name;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+    }
 }
