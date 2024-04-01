f5af068394acc665d3aa8a19a7a901c9b6dbbe0b
arianaaghajani <aghajaniariana@gmail.com>
2024-04-01 18:50:35 +0430

    fix StandardServiceRegistry and factory service

--- src/main/java/ir/ariana/repository/PersonRepository.java
+++ src/main/java/ir/ariana/repository/PersonRepository.java
@@ -1,4 +1,23 @@
 package ir.ariana.repository;
 
+
+import ir.ariana.q2.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 public class PersonRepository {
+    StandardServiceRegistry build = new StandardServiceRegistryBuilder()
+            .configure()
+            .build();
+
+    SessionFactory sessionFactory = new MetadataSources(build)
+            .addAnnotatedClass(Person.class)
+            .buildMetadata()
+            .buildSessionFactory();
+
+
 }
