1573f14b20b6144edd411d6d0831170111e0396f
arianaaghajani <aghajaniariana@gmail.com>
2024-04-01 18:09:49 +0430

    add objects and lombok annotation

--- src/main/java/ir/ariana/q2/Person.java
+++ src/main/java/ir/ariana/q2/Person.java
@@ -1,6 +1,23 @@
 package ir.ariana.q2;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Person {
+    @Id
+    private Integer id;
+    private String name;
+    private String lastName;
+    private String birthDate;
 
 
 }
