935a05e58156e6d5c68fc149c9a04dfbc45c2c54
arianaaghajani <aghajaniariana@gmail.com>
2024-04-02 12:29:16 +0430

    fix save filed

--- src/main/java/ir/ariana/q2/Service/PersonService.java
+++ src/main/java/ir/ariana/q2/Service/PersonService.java
@@ -1,4 +1,12 @@
 package ir.ariana.q2.Service;
 
+import ir.ariana.q2.Person;
+import ir.ariana.repository.PersonRepository;
+
 public class PersonService {
+    public final PersonRepository personRepository= new PersonRepository();
+
+    public void save(Person person){
+        personRepository.save(person);
+    }
 }
