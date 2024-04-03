0187078910853c346da23e67693cc4e29d4b2433
arianaaghajani <aghajaniariana@gmail.com>
2024-04-03 22:40:10 +0430

    add save method

--- src/main/java/ir/ariana/repository/TeacherRepository.java
+++ src/main/java/ir/ariana/repository/TeacherRepository.java
@@ -1,5 +1,15 @@
 package ir.ariana.repository;
 
-public class TeacherRepository {
+import ir.ariana.q2.model.Teacher;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+public abstract class TeacherRepository extends PersonRepository{
+    public void save(Teacher teacher) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(teacher);
+        transaction.commit();
+        session.close();
+    }
 }
