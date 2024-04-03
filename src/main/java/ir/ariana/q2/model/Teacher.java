cf1dfc4741c3d7fff0a25d865606e3a51b2379e4
arianaaghajani <aghajaniariana@gmail.com>
2024-04-03 16:15:24 +0430

    add constructor

--- src/main/java/ir/ariana/q2/model/Teacher.java
+++ src/main/java/ir/ariana/q2/model/Teacher.java
@@ -2,6 +2,8 @@ package ir.ariana.q2.model;
 import jakarta.persistence.Entity;
 import lombok.*;
 
+import java.util.Date;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -21,8 +23,13 @@ public class Teacher extends Person {
         PROFESSOR
     }
 
-
-
-
-
+    public Teacher(Long id, String name, String lastName, Date birthDate, Long id1, String teacherId,
+                   String degreeOfEducation, String teacherGrade, double monthlySalary) {
+        super(id, name, lastName, birthDate);
+        this.id = id1;
+        this.teacherId = teacherId;
+        this.degreeOfEducation = degreeOfEducation;
+        this.teacherGrade = teacherGrade;
+        this.monthlySalary = monthlySalary;
+    }
 }
