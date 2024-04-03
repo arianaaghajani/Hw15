e1a5239b88739465ecc5074420a86c2f437dba9e
arianaaghajani <aghajaniariana@gmail.com>
2024-04-03 22:43:21 +0430

    add find by id method

--- src/main/java/ir/ariana/repository/TeacherRepository.java
+++ src/main/java/ir/ariana/repository/TeacherRepository.java
@@ -1,5 +1,6 @@
 package ir.ariana.repository;
 
+import ir.ariana.q2.model.Person;
 import ir.ariana.q2.model.Teacher;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -11,5 +12,14 @@ public abstract class TeacherRepository extends PersonRepository{
         session.persist(teacher);
         transaction.commit();
         session.close();
+    }
+
+    public Teacher findById(Long id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Teacher teacher = session.find(Teacher.class, id);
+        transaction.commit();
+        session.close();
+        return teacher;
     }
 }
